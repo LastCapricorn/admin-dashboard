d55d7d01a3ad0968efe73da3b8d3f15840e98aed
Last Capricorn <48774366+LastCapricorn@users.noreply.github.com>
2023-04-28 15:57:15 +0200

    Make it responsive
    - description in README

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# admin-dashboard
-2nd project of TOP's Full Stack JavaScript - Path 
+# Admin - Dashboard
+
+## 2nd project of TOP's Full Stack JavaScript Path
+
+This is my "No-Flex-Box-At-All"-Solution
+
++ Even the "Grid Logo" is grid
+
+  and of course it is a button to collapse the navbar
+
++ I used the "Space Grotesk" font from google fonts
+
+  <https://fonts.google.com/specimen/Space+Grotesk?query=space>
+
++ and the icon set is "Phosphor"
+
+  <https://phosphoricons.com>
+
++ I gave this one a bit of responsiveness, hope it'll work.
+
+This project was predestined to present my previous projects for TOP and so I did. It also will be good for the upcoming projects.
+
+I'm quite happy with my work, although it is truly expandable - keyword: accessibility...
+
+I really hope you like the design.
+
+I'm always in doubt about the quality of my English - maybe it's good for a few laughs?
--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-document.querySelector('div[role="button"]').addEventListener('click', toggleNav);
+function checkScreenWidth() {
+  if (document.documentElement.clientWidth < 703) {
+    document.querySelector('body').classList.add('collapsed');
+    document.querySelector('nav').classList.add('collapsed', 'fixed');
+  } else {
+    document.querySelector('nav').classList.remove('fixed');
+  }
+}
 
 function toggleNav() {
-  document.querySelector('body').classList.toggle('collapsed');
-  document.querySelector('nav').classList.toggle('collapsed');
+  if (!document.querySelector('nav').classList.contains('fixed')) {
+    document.querySelector('body').classList.toggle('collapsed');
+    document.querySelector('nav').classList.toggle('collapsed');
+  }
 }
+
+checkScreenWidth()
+
+document.querySelector('div[role="button"]').addEventListener('click', toggleNav);
+window.addEventListener('resize', checkScreenWidth);
