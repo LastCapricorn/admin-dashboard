87c426992ce28de5cee7c0ba6a4082e439d2212f
Last Capricorn <48774366+LastCapricorn@users.noreply.github.com>
2023-05-10 14:12:05 +0200

    Add project Library
    - add library screenshot
    - JS prettier

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-function checkScreenWidth() {
+function checkScreenWidth () {
   if (document.documentElement.clientWidth < 703) {
-    document.querySelector('body').classList.add('collapsed');
-    document.querySelector('nav').classList.add('collapsed', 'fixed');
+    document.querySelector('body').classList.add('collapsed')
+    document.querySelector('nav').classList.add('collapsed', 'fixed')
   } else {
-    document.querySelector('nav').classList.remove('fixed');
+    document.querySelector('nav').classList.remove('fixed')
   }
 }
 
-function toggleNav() {
+function toggleNav () {
   if (!document.querySelector('nav').classList.contains('fixed')) {
-    document.querySelector('body').classList.toggle('collapsed');
-    document.querySelector('nav').classList.toggle('collapsed');
+    document.querySelector('body').classList.toggle('collapsed')
+    document.querySelector('nav').classList.toggle('collapsed')
   }
 }
 
 checkScreenWidth()
 
-document.querySelector('div[role="button"]').addEventListener('click', toggleNav);
-window.addEventListener('resize', checkScreenWidth);
+document
+  .querySelector('div[role="button"]')
+  .addEventListener('click', toggleNav)
+window.addEventListener('resize', checkScreenWidth)
